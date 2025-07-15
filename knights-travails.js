ac3f241aac7561e99bdc006e6144bda0b7e883ce
Rohit Bhojak <rohitbhojak99@gmail.com>
2025-07-15 06:05:37 +0000

    Complete knight travails and add comments

--- knights-travails.js
+++ knights-travails.js
@@ -1,8 +1,10 @@
 export default function knightMoves(start, end) {
+  // Check validity of arguments
   if (!isValid(start) || !isValid(end)) {
-    return "Enter valid starting and ending coordinates";
+    throw new Error("Enter valid starting and ending coordinates");
   }
 
+  // Initialize visited matrix to false
   const visited = [];
   for (let i = 0; i < 8; i++) {
     const arr = [];
@@ -10,40 +12,78 @@ export default function knightMoves(start, end) {
     visited.push(arr);
   }
 
+  // Initialize map to keep track of parent coordinates
+  const parent = new Map();
+
+  // Moves array
   const rowMoves = [2, 2, -2, -2, 1, 1, -1, -1];
   const colMoves = [1, -1, 1, -1, 2, -2, 2, -2];
 
+  // Initialize queue with start and mark visited
   const queue = [];
-  queue.push([start[0], start[1]]);
-  visited[start[0]][start[1]] = true;
-  let count = 0;
+  visitSquare(queue, visited, parent, null, start); //set start parent to null
 
+  // Initialize total moves to zero
+  let moves = 0;
+
+  // While queue is not empty
   while (queue.length) {
     let size = queue.length;
-    count++;
-    while (size--) {
-      const [row, col] = queue.shift();
-      if (row === end[0] && col === end[1]) return count;
 
+    while (size--) {
+      // Dequeue square
+      const [row, col] = queue.shift();
+      const square = [row, col];
+
+      // Get path if end is reached
+      if (square[0] === end[0] && square[1] === end[1]) {
+        console.log(`You made it in ${moves} moves!  Here's your path:`);
+        getPath(parent, square);
+        return;
+      }
+
+      // Loop to move the knight
       for (let i = 0; i < 8; i++) {
         const newRow = row + rowMoves[i];
         const newCol = col + colMoves[i];
+        const newSquare = [newRow, newCol];
 
-        if (isValid([newRow, newCol]) && !visited[newRow][newCol]) {
-          queue.push([newRow, newCol]);
-          visited[newRow][newCol] = true;
+        // If move is valid visit square
+        if (isValid(newSquare) && !visited[newRow][newCol]) {
+          visitSquare(queue, visited, parent, square, newSquare);
         }
       }
     }
+
+    // Increment moves
+    moves++;
   }
-  return -1;
 }
 
-function isValid(coordinates) {
-  if (!Array.isArray(coordinates) || coordinates.length !== 2) return false;
-  let [row, col] = coordinates;
+// A square is valid iff it's an array of length 2 with values in the range [0, 7];
+function isValid(square) {
+  if (!Array.isArray(square) || square.length !== 2) return false;
   for (let i = 0; i < 2; i++) {
-    if (coordinates[i] < 0 || coordinates[i] >= 8) return false;
+    if (square[i] < 0 || square[i] >= 8) return false;
   }
   return true;
+}
+
+// Pushes a square to queue, marks it as visited and set its parent
+function visitSquare(queue, visited, parent, from, to) {
+  queue.push(to);
+  visited[to[0]][to[1]] = true;
+  parent.set(JSON.stringify(to), JSON.stringify(from));
+}
+
+// Logs the path recursively
+function getPath(parent, square) {
+  const parentSquare = JSON.parse(parent.get(JSON.stringify(square)));
+  if (parentSquare === null) {
+    console.log(square);
+    return;
+  }
+
+  getPath(parent, parentSquare);
+  console.log(square);
 }
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import knightMoves from "./knights-travails.js";
 
-console.log(knightMoves([0, 0], [7, 7]));
-console.log(knightMoves([3, 3], [0, 0]));
-console.log(knightMoves([0, 0], [3, 3]));
+knightMoves([0, 0], [7, 7]);
+knightMoves([3, 3], [4, 3]);
